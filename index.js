ad371309762bb9387f7fc46b3c8fdba6e9d8cc48
kate <under5frisk@gmail.com>
2025-07-07 19:19:52 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let company = {
+  sales: [
+    { name: 'John', salary: 1000 },
+    { name: 'Alice', salary: 600 },
+  ],
+  development: {
+    web: [
+      { name: 'Peter', salary: 2000 },
+      { name: 'Alex', salary: 1800 },
+    ],
+    internals: [{ name: 'Jack', salary: 1300 }],
+  },
+
+  salaryCounter() {
+    let allEmployees = [
+      ...this.sales,
+      ...this.development.web,
+      ...this.development.internals,
+    ];
+    let allSalaries = allEmployees.map((el) => el.salary);
+    return allSalaries.reduce((acc, el) => {
+      return acc + el;
+    });
+  },
+};
+
+console.log(company.salaryCounter());
