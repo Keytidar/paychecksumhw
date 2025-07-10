7facf1534593d9fdbb954622307a56b7e562cbb0
kate <under5frisk@gmail.com>
2025-07-10 17:46:49 +0300

    final beta

--- index.js
+++ index.js
@@ -10,18 +10,29 @@ let company = {
     ],
     internals: [{ name: 'Jack', salary: 1300 }],
   },
+};
+const companyFunctions = {
+  collectEmployeeData(el) {
+    const result = [];
+    const values = Object.values(el);
+    values.forEach((element) => {
+      if (Array.isArray(element)) result.push(element);
+      if (typeof element === 'object' && !Array.isArray(element))
+        result.push(this.collectEmployeeData(element));
+    });
+    return result.flat(Infinity).filter((el) => typeof el === 'object');
+  },
 
-  salaryCounter() {
-    let allEmployees = [
-      ...this.sales,
-      ...this.development.web,
-      ...this.development.internals,
-    ];
-    let allSalaries = allEmployees.map(el => el.salary);
-    return allSalaries.reduce((acc, el) => {
+  countSalary(input) {
+    const salaries = input.map((el) => el.salary);
+    return salaries.reduce((acc, el) => {
       return acc + el;
     });
   },
 };
 
-console.log(company.salaryCounter());
+console.log(companyFunctions.collectEmployeeData(company));
+
+console.log(
+  companyFunctions.countSalary(companyFunctions.collectEmployeeData(company))
+);
